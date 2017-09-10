cd48a6468b4384df6049967358f90ee8c4accabc
jayanthg11 <jayanthg11@gmail.com>
2017-09-11 02:06:02 +0530

    u solve the errors

---
+++ app/src/main/java/com/debut/ellipsis/freehit/Stats/Playerpackage/CustomGridView.java
@@ -0,0 +1,66 @@
+package com.debut.ellipsis.freehit.Stats.Playerpackage;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.debut.ellipsis.freehit.R;
+
+/**
+ * Created by Jayanth on 11-09-2017.
+ */
+
+public class CustomGridView extends BaseAdapter {
+    private Context mContext;
+    private final String[] gridViewString;
+    //private final int[] gridViewImageId;
+
+    public CustomGridView(Context context, String[] gridViewString) {
+        mContext = context;
+        // this.gridViewImageId = gridViewImageId;
+        this.gridViewString = gridViewString;
+    }
+
+    @Override
+    public int getCount() {
+        return gridViewString.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+        View gridViewAndroid;
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+
+            gridViewAndroid = new View(mContext);
+
+            gridViewAndroid = inflater.inflate(R.layout.grid_view_player_adapter, null);
+            TextView textViewAndroid = (TextView) gridViewAndroid.findViewById(R.id.android_gridview_text);
+            //ImageView imageViewAndroid = (ImageView) gridViewAndroid.findViewById(R.id.android_gridview_image);
+            textViewAndroid.setText(gridViewString[i]);
+            // imageViewAndroid.setImageResource(gridViewImageId[i]);
+        } else {
+            gridViewAndroid = (View) convertView;
+        }
+
+        return gridViewAndroid;
+    }
+
+
+}
--- app/src/main/java/com/debut/ellipsis/freehit/Stats/Playerpackage/Info_Fragment.java
+++ app/src/main/java/com/debut/ellipsis/freehit/Stats/Playerpackage/Info_Fragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.debut.ellipsis.freehit.R;
 
@@ -12,12 +13,23 @@ import com.debut.ellipsis.freehit.R;
  * Created by Jayanth on 10-09-2017.
  */
 
-public class Info_Fragment extends Fragment {
+public class Info_Fragment extends Fragment {GridView androidGridView;
+
+    String[] gridViewString = {
+            "   ","TEST","ODI","T20I","Bat","1","1","1",
+            "2","2","2",
+            "1","1","1",
+
+    } ;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_stats_info, container, false);
+        CustomGridView adapterViewAndroid = new CustomGridView(Info_Fragment.this, gridViewString );
+        androidGridView=(GridView)findViewById(R.id.grid_view_image_text);
+        androidGridView.setAdapter(adapterViewAndroid);
 
         return rootView;
     }
